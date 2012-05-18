3db445492839178e12be5c74f2a279f70f482c7b
Derek Lindahl <dlindahl@customink.com>
2012-05-17 21:05:36 -0400

    Add ability to specify compilation options.
    Specifically, this allows the toggling of the `bare` option.

--- tasks/coffee.js
+++ tasks/coffee.js
@@ -31,15 +31,20 @@ module.exports = function(grunt) {
   // HELPERS
   // ==========================================================================
 
-  grunt.registerHelper('coffee', function(src, destPath) {
+  grunt.registerHelper('coffee', function(src, destPath, options) {
     var coffee = require('coffee-script'),
         js = '';
 
     var dest = path.join(destPath, 
                          path.basename(src, '.coffee') + '.js');
 
+    options = options || {};
+    if( options.bare !== false ) {
+      options.bare = true;
+    }
+
     try {
-      js = coffee.compile(grunt.file.read(src), { bare: true });
+      js = coffee.compile(grunt.file.read(src), options);
       grunt.file.write(dest, js);
     } catch (e) {
       grunt.log.error("Unable to compile your coffee", e);
